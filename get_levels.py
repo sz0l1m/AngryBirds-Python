42d3b91cb60f8ded59d80df1e3ab3f685394c077
mandrysz <01187204@pw.edu.pl>
2023-12-29 16:22:09 +0100

    Created Level class

--- get_levels.py
+++ get_levels.py
@@ -6,5 +6,21 @@ def get_data():
         return json.load(fp)
 
 
-data = get_data()
-print(data["levels"][0]["objects"]["pigs"])
+levels_data = get_data()["levels"]
+
+
+class Level:
+    """
+    Class Level. Contains attributes:
+    :param number: number of the level
+    :type number: int
+
+    :param objects: all objects of the level
+    :type objects: dict
+    """
+    def __init__(self, level_data: dict):
+        """
+        Creates instance of the level.
+        """
+        self._number = level_data["level"]
+        self._objects = level_data["objects"]
