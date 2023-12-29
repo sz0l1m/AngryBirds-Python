3cec32a52c1bcbe995a199be939be17d2afb664d
mandrysz <01187204@pw.edu.pl>
2023-12-29 18:53:00 +0100

    Added StrinIO to test_get_levels

--- test_get_levels.py
+++ test_get_levels.py
@@ -1,10 +1,58 @@
 from get_levels import (
-    Level,
-    get_data
+    Level
 )
+from io import StringIO
+import json
 
 
-data = get_data()['levels']
+file = """
+{
+    "levels": [
+        {
+            "level": 1,
+            "objects": {
+                "birds": {
+                    "amount": 2
+                },
+                "pigs": [
+                    {
+                        "x_position": 800,
+                        "y_position": 320,
+                        "radius": 20
+                    },
+                    {
+                        "x_position": 850,
+                        "y_position": 120,
+                        "radius": 30
+                    }
+                ],
+                "bars": [
+                    {
+                        "x_position": 700,
+                        "y_position": 200,
+                        "x_size": 20,
+                        "y_size": 200
+                    },
+                    {
+                        "x_position": 900,
+                        "y_position": 200,
+                        "x_size": 20,
+                        "y_size": 200
+                    },
+                    {
+                        "x_position": 800,
+                        "y_position": 310,
+                        "x_size": 220,
+                        "y_size": 20
+                    }
+                ]
+            }
+        }
+    ]
+}
+"""
+file_handle = StringIO(file)
+data = json.load(file_handle)['levels']
 
 
 def test_level_create():
