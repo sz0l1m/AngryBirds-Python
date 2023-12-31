b93a41828c0c99f6d2f96ecd54bbcdba57f35b48
mandrysz <01187204@pw.edu.pl>
2023-12-31 14:45:22 +0100

    Fixed bug when pig is out of screen, now its killed

--- game.py
+++ game.py
@@ -27,11 +27,14 @@ import collisions
 def handle_level(space: pymunk.Space, level: Level):
     pigs = 0
     for body, shape in zip(space.bodies, space.shapes):
+        if shape.collision_type == 3:
+            if body.position[0] > SCREEN_WIDTH + 50 or body.position[0] < -50:
+                space.remove(body, shape)
         if round(body.velocity[0]) != 0 or round(body.velocity[1]) != 0:
             return None
         if shape.collision_type == 3:
             pigs += 1
-    if pigs == 0:
+    if pigs == 0 and level.number < level.amount_of_levels:
         return 'Next level'
 
 
