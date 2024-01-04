50012f21f810773bf109418c0a534b1ab6067855
mandrysz <01187204@pw.edu.pl>
2024-01-04 11:12:32 +0100

    Updated FPS and bird_radius in config

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ pygame.init()
 pygame_info = pygame.display.Info()
 SCREEN_WIDTH = pygame_info.current_w - 7
 SCREEN_HEIGHT = pygame_info.current_h - 30
-FPS = 40
+FPS = 30
 gravity = (0, -500)
-bird_radius = 30
+bird_radius = 20
 bird_position = (220, 200 + bird_radius)
 floor_height = 200
 aiming_range = 200
