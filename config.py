12997730c7fb00adfcea9c7ed13874d3d6de51a7
mandrysz <01187204@pw.edu.pl>
2024-01-06 14:08:24 +0100

    Added screen_factor in config.py

--- config.py
+++ config.py
@@ -7,15 +7,18 @@ def get_screen_size():
     height = screen.current_h
     print(width / height)
     if width / height > 16 / 9:
-        return (1920 * (height / 1080), height)
+        factor = height / 1080
+        return (1920 * factor, height, factor)
     else:
-        return (width, 1080 * (width / 1920))
+        factor = width / 1920
+        return (width, 1080 * factor, factor)
 
 
 pygame.init()
 screen = get_screen_size()
 SCREEN_WIDTH = int(screen[0]) - 7
 SCREEN_HEIGHT = int(screen[1]) - 30
+screen_factor = screen[2]
 FPS = 30
 gravity = (0, -500)
 bird_radius = 20
